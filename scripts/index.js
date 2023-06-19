1bead2fd74f16be54631a18cd7b59c404eb35dc0
jacquistapace <jacquistapace8@gmail.com>
2023-06-19 00:01:08 -0300

    add new library to get countries phone number codes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $('#txtPhone').countrySelect({
-        defaultCountry: "pl"
+    $('#txtPhone').intlTelInput({
+        initialCountry: "PL"
     });
 });
+
+const sendMessage = (event) => {
+    event.preventDefault();
+    console.log("🚀 ~ file: index.js:16 ~ sendMessage ~ message.event.target.phone.value:", event.target.phone.value)
+    const message = {
+        firstName: event.target.firstName.value,
+        lastName: event.target.lastName.value,
+        company: event.target.company.value,
+        phone: event.target.phone.value,
+        message: event.target.message.value,
+    }
+    console.log("🚀 ~ file: index.js:15 ~ sendMessage ~ message:", message)
+    console.log("🚀 ~ file: index.js:8 ~ sendMessage ~ event:", event);
+}
